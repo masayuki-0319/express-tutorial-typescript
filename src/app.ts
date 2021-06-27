aabdcbbec66abd40f8c61acec7caabfe47a502a7
将之大西 <omccello@gmail.com>
2021-06-27 12:12:42 +0900

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,19 @@
 import express from 'express';
+import { router } from './routes/todos';
 
 const app = express();
+
+app.use('/todos', router);
+
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    res.status(500).json({ message: err.message });
+  }
+);
+
 app.listen(3000);
