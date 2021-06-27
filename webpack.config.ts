bc6b99c7a0c787b0ad7d7e7e2ae8bb51d317d922
将之大西 <omccello@gmail.com>
2021-06-28 00:46:11 +0900

    fix

---
+++ webpack.config.ts
@@ -0,0 +1,27 @@
+import { Configuration } from 'webpack';
+import path from 'path';
+
+const config: Configuration = {
+  mode: 'development',
+  target: 'node',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
+
+export default config;
